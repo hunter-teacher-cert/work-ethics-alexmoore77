39b12ba361ce1802234518150eba7f049791ab37
Alexander Moore <77898412+alexmoore77@users.noreply.github.com>
2021-09-22 20:54:48 +0000

    Elicited user info

--- week_01_python/Chess.py
+++ week_01_python/Chess.py
@@ -3,7 +3,8 @@
 # Beginning Chess:  2-Player Edition is designed to allow two players to enjoy the wonderful game of chess!
 
 #initilizing board globally so it does not need to be passed among functions
-
+player1=""
+player2=""
 board=[
   ['R','N','B','Q','K','B','N','R'],
   ['P','P','P','P','P','P','P','P'],
@@ -17,17 +18,18 @@ board=[
 
 def drawBoard(  ):
   print('\n\n\n\n\n\n\n\n\n\nWelcome to Beginning Chess!\n')
-  #print('What is your name?')    # ask for their name
-  #myName = input()
-  #print('It is good to meet you, ' + myName)
-  #print('The length of your name is:')
-  #print(len(myName))
-  #print('What is your age?')    # ask for their age
-  #myAge = input()
-  #print('You will be ' + str(int(myAge) + 1) + ' in a year.')
   counter=8
   for (x) in board:
     print(x)
   print('\n\n\n\n\n\n\n\n\n\n');
 
+def getUserInfo():
+  print('Please enter the name of player 1:')
+  player1=input()
+  print('Welcome, '+ player1 +'!  Now enter the name of player 2.')
+  player2=input();
+  
+
+
 drawBoard()
+getUserInfo()
