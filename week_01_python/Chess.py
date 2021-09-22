7e12a11069a460d046b2c1c7d87fa67c9fc26707
Alexander Moore <77898412+alexmoore77@users.noreply.github.com>
2021-09-22 15:16:54 +0000

    Added chess board and list

---
+++ week_01_python/Chess.py
@@ -0,0 +1,27 @@
+# Alex Moore 
+# git hub alexmoore77
+# Beginning Chess:  2-Player Edition is designed to allow two players to enjoy the wonderful game of chess!
+
+print('\n\n\n\n\n\n\n\n\n\nWelcome to Beginning Chess!\n')
+#print('What is your name?')    # ask for their name
+#myName = input()
+#print('It is good to meet you, ' + myName)
+#print('The length of your name is:')
+#print(len(myName))
+#print('What is your age?')    # ask for their age
+#myAge = input()
+#print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+board=[
+  ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],  
+  ['P','P','P','P','P','P','P','P'],
+  ['R','N','B','Q','K','B','N','R']
+]
+counter=8
+for (x) in board:
+  print(x)
+print('\n\n\n\n\n\n\n\n\n\n')
