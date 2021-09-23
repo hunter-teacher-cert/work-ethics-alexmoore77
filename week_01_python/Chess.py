8588ec9dc3739ad41f12628f460d54669a26e714
Alexander Moore <77898412+alexmoore77@users.noreply.github.com>
2021-09-23 00:30:22 +0000

    Allowed multiple players to select squares, and added the modifySquare() function.

--- week_01_python/Chess.py
+++ week_01_python/Chess.py
@@ -17,19 +17,71 @@ board=[
 ]
 
 def drawBoard(  ):
-  print('\n\n\n\n\n\n\n\n\n\nWelcome to Beginning Chess!\n')
+  print('\n\n\n\n\n\n\n\n\n\nBeginning Chess!\n')
+  print('    A   B   C   D   E   F   G   H   I   J')
   counter=8
   for (x) in board:
-    print(x)
+    print(str(counter)+' '+str(x))
+    counter=counter-1
   print('\n\n\n\n\n\n\n\n\n\n');
 
-def getUserInfo():
-  print('Please enter the name of player 1:')
-  player1=input()
-  print('Welcome, '+ player1 +'!  Now enter the name of player 2.')
-  player2=input();
-  
-
+def modifySquare(myLetter, myNumber, myChar ):
+  if myLetter=='A':
+    myLetterConverted=0
+  if myLetter=='B':
+    myLetterConverted=1
+  if myLetter=='C':
+    myLetterConverted=2
+  if myLetter=='D':
+    myLetterConverted=3
+  if myLetter=='E':
+    myLetterConverted=4
+  if myLetter=='F':
+    myLetterConverted=5
+  if myLetter=='G':
+    myLetterConverted=6
+  if myLetter=='H':
+    myLetterConverted=7
+  board[myNumber, myLetterConverted]=myChar
 
 drawBoard()
-getUserInfo()
+
+print('Please enter the name of player 1:')
+player1=input()
+print('Welcome, '+ player1 +'!  Now enter the name of player 2.')
+player2=input()
+print('\n\n\n\n\n\n\n\n\n\nWelcome to Beginning Chess!\n')
+
+drawBoard()
+
+
+turn=0
+
+#take turns
+while (1==1):
+  
+  #Identify whose turn it is
+  turn=turn+1
+  if turn % 2 == 0:
+    thisPlayer=player1
+  if turn %2 ==1:
+     thisPlayer=player2
+  print ('The turn is '+str(turn)+'.  The player is '+thisPlayer)
+  drawBoard();
+  print (thisPlayer+ ', from what square will you move your piece?')
+  start=input()
+  print (thisPlayer+ ', to what square would you like to move this piece?')
+  destination=input()
+  print('Move: '+start+' to '+destination)
+  startLetterFrom=start[:1]
+  startNumberFrom=start[1:2]
+  destinationLetterTo=destination[:1]
+  destinationNumberTo=destination[1:2]
+  print('startLetterFrom: '+startLetterFrom)
+  print('startNumberFrom: '+startNumberFrom)
+  print('destinationLetterTo: '+destinationLetterTo)
+  print('destinationNumberTo: '+destinationNumberTo)
+  
+  
+  #modifySquare(startLetterFrom, startNumberFrom, ' ')
+  #modifySquare(destinationLetterTo, destinationNumberTo, 'P')
